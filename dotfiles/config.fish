ddaa4b19d03b945489f40163bb4b3d969a6ee938
regular-nick <ngtelitsyn@disroot.org>
2025-02-20 23:24:47 +0100

    updated config.fish to run ssh-agent

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fastfetch --logo nix_old_small
+    # fastfetch --logo nix_old_small
+    eval (ssh-agent -c)
+    ssh-add
     starship init fish | source
     any-nix-shell fish --info-right | source
     direnv hook fish | source
