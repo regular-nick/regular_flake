1bd57ed0405b20edaf05e33f4b4af071ddfb466e
Forecastr-sci <ngtelitsyn@disroot.org>
2025-02-21 23:01:11 +0100

    first successful unstable version; need to extract home-manager in flake to avoid warnings

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Minimal example of Nix-on-Droid system config with home-manager.";
 
   inputs = {
+
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     
@@ -15,26 +16,24 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
-
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/master";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-      inputs.home-manager.follows = "home-manager-unstable";
+      url = "github:nix-community/nix-on-droid/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
-  };
-
 
     nix-on-droid-unstable = {
       url = "github:nix-community/nix-on-droid/master";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.home-manager.follows = "home-manager-unstable";
     };
+
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-on-droid }: {
+  outputs = inputs@{ self, ... } : {
 
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
+    nixOnDroidConfigurations.default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import inputs.nixpkgs-unstable { system = "aarch64-linux"; };
       modules = [ ./nix-on-droid.nix ];
     };
 
