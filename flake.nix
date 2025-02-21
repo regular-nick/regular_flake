378535dbc64565a78660f2a4c1f06c57656ddf9f
regular-nick <ngtelitsyn@disroot.org>
2025-02-21 21:13:13 +0100

    switched to unstable home-manager & nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,24 +2,30 @@
   description = "Minimal example of Nix-on-Droid system config with home-manager.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      url = "github:nix-community/nix-on-droid/master";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.home-manager.follows = "home-manager-unstable";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-on-droid }: {
+  outputs = { self, nixpkgs-unstable, home-manager-unstable, nix-on-droid }: {
 
     nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      pkgs = import nixpkgs-unstable { system = "aarch64-linux"; };
       modules = [ ./nix-on-droid.nix ];
     };
 
